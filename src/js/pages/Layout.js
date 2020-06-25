ea145c576840f6c3ed2553380d231ad68c735966
TokiyaHorikawa <ayikot4423@gmail.com>
2020-06-25 22:07:36 +0900

    feat: add router

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default class Layout extends React.Component {
   render() {
     return (
-      <h1>KillerNews.net</h1>
+      <div>
+        <h1>KillerNews.net</h1>
+        {this.props.children}
+        <Link to="/archives"><button class="btn btn-danger">archives</button></Link>
+        <Link to="/settings"><button class="btn btn-success">settings</button></Link>
+      </div>
     );
   }
 }
