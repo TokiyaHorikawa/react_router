d16e77260158d2371eabf6c379ea0580a13b75c0
TokiyaHorikawa <ayikot4423@gmail.com>
2020-06-25 22:36:46 +0900

    feat: router history

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
-export default class Layout extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>KillerNews.net</h1>
-        {this.props.children}
-        <Link to="/archives"><button class="btn btn-danger">archives</button></Link>
-        <Link to="/settings"><button class="btn btn-success">settings</button></Link>
-      </div>
-    );
+class Layout extends React.Component {
+  navigate() {
+    console.log(this.props.history);
+    this.props.history.push("/");
   }
-}
+  render() {
+     return (
+       <div>
+         <h1>KillerNews.net</h1>
+         {this.props.children}
+         <Link to="/archives" className="btn btn-danger">archives</Link>
+         <Link to="/settings" className="btn btn-success">settings</Link>
+        <button className="btn btn-info" onClick={this.navigate.bind(this)}>featured</button>
+       </div>
+     );
+   }
+ }
+
+ export default withRouter(Layout);
