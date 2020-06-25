ef89e93731cf0fd0860dfc49d9573ac86a84dafe
TokiyaHorikawa <ayikot4423@gmail.com>
2020-06-25 22:07:49 +0900

    feat: add react html

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
       use: [{
         loader: 'babel-loader',
         options: {
+          plugins: ['react-html-attrs'],
           presets: ['@babel/preset-react', '@babel/preset-env']
         }
       }]
